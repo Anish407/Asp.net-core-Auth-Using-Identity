7705101f745a9bc20a3f04563cb598d87a6a6073
Anish Aravind <anish.arvind407@gmail.com>
2020-03-21 19:10:56 +0530

    Add project files.

---
+++ ConfArch.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ConfArch.Web.Areas.Identity.Data;
+using ConfArch.Web.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ConfArch.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ConfArch.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ConfArchWebContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ConfArchWebContextConnection")));
+
+                services.AddDefaultIdentity<ConfArchWebUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ConfArchWebContext>();
+            });
+        }
+    }
+}
